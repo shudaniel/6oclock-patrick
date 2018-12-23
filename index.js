de61f0bec24fd3368a74e0868dd55924f4076a34
Daniel Shu <danielshu98@gmail.com>
2018-12-23 13:38:06 -0800

    Changed post message. added hashtags

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function checkTime()
     var d = new Date(); // current time
     var hours = d.getHours();
 
-    return hours == 18 
+    return (hours == 18) 
 }
 
 var writeTweet = function() 
@@ -30,7 +30,7 @@ var writeTweet = function()
       T.post('media/metadata/create', meta_params, function (err, data, response) {
         if (!err) {
           // now we can reference the media and post a tweet (media will attach to the tweet)
-          var params = { status: 'Time for Patrick to come home and change Junior\'s diaper.', media_ids: [mediaIdStr] }
+          var params = { status: 'Time for Patrick to come home and change Junior\'s diaper. #spongebob #patrick #rockabyebivalve', media_ids: [mediaIdStr] }
 
           T.post('statuses/update', params, function (err, data, response) {
             console.log(data)
@@ -63,4 +63,4 @@ async function run()
     }
 }
 
-run()
+// run()
